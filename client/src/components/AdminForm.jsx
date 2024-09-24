5f5f7944ae0ced8417ba568f2513300bc3c18d0c
Srinath Bondala <srinath.b2004@gmail.com>
2024-09-24 15:16:00 +0530

    created server endpoint for user and admin

--- client/src/components/AdminForm.jsx
+++ client/src/components/AdminForm.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { TextField, Button, Box, Typography, MenuItem, Chip, IconButton, Grid, Snackbar } from '@mui/material';
+import { TextField, Button, Box, Typography, MenuItem, Chip, InputAdornment, IconButton, Grid } from '@mui/material';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import AddIcon from '@mui/icons-material/Add';
 import { AddCircleOutline } from '@mui/icons-material';
 
 const Form = () => {
+
     const [companyName, setCompanyName] = useState('');
     const [role, setRole] = useState('');
     const [experience, setExperience] = useState('');
@@ -12,9 +13,7 @@ const Form = () => {
     const [location, setLocation] = useState('');
     const [skills, setSkills] = useState([]);
     const [skillInput, setSkillInput] = useState('');
-    const [description, setDescription] = useState('');
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [employmenttype,setEmploymentType]=useState('');
 
     const handleAddSkill = () => {
         if (skillInput.trim() && !skills.includes(skillInput)) {
@@ -23,168 +22,161 @@ const Form = () => {
         }
     };
 
+
     const handleDeleteSkill = (skillToDelete) => {
         setSkills(skills.filter(skill => skill !== skillToDelete));
     };
 
+    // Experience and Salary range options
     const experienceRanges = ['0-1 years', '2-3 years', '3-5 years', '5+ years'];
     const salaryRanges = ['2-3 LPA', '4-5 LPA', '6-9 LPA', '10+ LPA'];
-
-    const validateForm = () => {
-        if (!companyName) return 'Company Name is required';
-        if (!role) return 'Role is required';
-        if (!location) return 'Location is required';
-        if (!experience) return 'Experience is required';
-        if (!salary) return 'Salary is required';
-        if (!description) return 'Description is required';
-        if (skills.length === 0) return 'At least one skill is required';
-        return null; 
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const errorMessage = validateForm();
-        if (errorMessage) {
-            setSnackbarMessage(errorMessage);
-            setSnackbarOpen(true);
-            return;
-        }
-
-        console.log({ companyName, role, experience, salary, location, skills });
-        setCompanyName('');
-        setRole('');
-        setExperience('');
-        setSalary('');
-        setLocation('');
-        setSkills([]);
-        setDescription('');
-        setSkillInput('');
-    };
-
-    const handleCloseSnackbar = () => {
-        setSnackbarOpen(false);
-    };
-
+    const employmentType=['Remote','Full Time','Part Time'];
     return (
         <Box sx={{ mx: 'auto', mt: 4, backgroundColor: "#f7faff", borderRadius: 2, padding: 2 }}>
-            <Typography variant='h6' sx={{ fontWeight: 'bold', textTransform: 'uppercase', display: "flex", alignItems: "center" }} gutterBottom>
+            <Typography variant='h6' sx={{ fontWeight: 'bold', textTransform: 'uppercase', backdropFilter: 'blur', display: "flex", alignItems: "center" }} gutterBottom>
                 New Application <NoteAddIcon />
             </Typography>
-            <form onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Company Name"
-                            name="companyName"
-                            fullWidth
-                            value={companyName}
-                            onChange={(e) => setCompanyName(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField
-                            label="Role"
-                            name="role"
-                            fullWidth
-                            value={role}
-                            onChange={(e) => setRole(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField
-                            label="Location"
-                            name="location"
-                            fullWidth
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Description"
-                            name="description"
-                            fullWidth
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            select
-                            label="Experience"
-                            name="experience"
-                            fullWidth
-                            value={experience}
-                            onChange={(e) => setExperience(e.target.value)}
-                        >
-                            {experienceRanges.map((range) => (
-                                <MenuItem key={range} value={range}>
-                                    {range}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            select
-                            label="Salary"
-                            name="salary"
-                            fullWidth
-                            value={salary}
-                            onChange={(e) => setSalary(e.target.value)}
-                        >
-                            {salaryRanges.map((range) => (
-                                <MenuItem key={range} value={range}>
-                                    {range}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Box>
-                            <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-                                <TextField
-                                    label="Technical Skills"
-                                    name="skills"
-                                    fullWidth
-                                    value={skillInput}
-                                    onChange={(e) => setSkillInput(e.target.value)}
-                                />
-                                <IconButton onClick={handleAddSkill}>
-                                    <AddCircleOutline />
-                                </IconButton>
-                            </Box>
-                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
-                                {skills.map((skill, index) => (
-                                    <Chip
-                                        key={index}
-                                        label={skill}
-                                        onDelete={() => handleDeleteSkill(skill)}
-                                    />
-                                ))}
-                            </Box>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            fullWidth
-                            sx={{ mt: 2, display: "flex", alignItems: "center", gap: 1 }}
-                        >
-                            <AddIcon /> ADD
-                        </Button>
-                    </Grid>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        label="Company Name"
+                        name="companyName"
+                        fullWidth
+                        value={companyName}
+                        onChange={(e) => setCompanyName(e.target.value)}
+                    />
                 </Grid>
-            </form>
-
-            <Snackbar
-                open={snackbarOpen}
-                autoHideDuration={6000}
-                onClose={handleCloseSnackbar}
-                message={snackbarMessage}
-            />
+                <Grid item xs={6}>
+                    <TextField
+                        label="Role"
+                        name="role"
+                        fullWidth
+                        value={role}
+                        onChange={(e) => setRole(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        label="Location"
+                        name="location"
+                        fullWidth
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        label="Description"
+                        name="description"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        label="Bond Details"
+                        name="bonddetails"
+                        fullWidth
+                    />
+                </Grid>
+             <Grid item xs={12} >
+                    <TextField
+                        select
+                        label="Employment Type"
+                        name="employmenttype"
+                        fullWidth
+                        value={employmenttype}
+                        onChange={(e) => setEmploymentType(e.target.value)}
+                    >
+                        {employmentType.map((range) => (
+                            <MenuItem key={range} value={range}>
+                                {range}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Grid>
+                <Grid item xs={12} >
+                    <TextField
+                        select
+                        label="Experience"
+                        name="experience"
+                        fullWidth
+                        value={experience}
+                        onChange={(e) => setExperience(e.target.value)}
+                    >
+                        {experienceRanges.map((range) => (
+                            <MenuItem key={range} value={range}>
+                                {range}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Grid>
+                <Grid item xs={12} >
+                    <TextField
+                        select
+                        label="Salary"
+                        name="salary"
+                        fullWidth
+                        value={salary}
+                        onChange={(e) => setSalary(e.target.value)}
+                    >
+                        {salaryRanges.map((range) => (
+                            <MenuItem key={range} value={range}>
+                                {range}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Grid>
+                <Grid item xs={12} >
+                    <Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
+                            <TextField
+                                label="Technical Skills"
+                                name="skills"
+                                fullWidth
+                                value={skillInput}
+                                onChange={(e) => setSkillInput(e.target.value)}
+                            />
+                            <IconButton onClick={handleAddSkill}>
+                                <AddCircleOutline />
+                            </IconButton>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+                            {skills.map((skill, index) => (
+                                <Chip
+                                    key={index}
+                                    label={skill}
+                                    onDelete={() => handleDeleteSkill(skill)}
+                                />
+                            ))}
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        label="Eligibility Criteria"
+                        name="eligibilityCriteria"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        label="Selection Process"
+                        name="selectionprocess"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} >
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        sx={{ mt: 2, display: "flex", alignItems: "center", gap: 1 }}
+                    >
+                        <AddIcon /> ADD
+                    </Button>
+                </Grid>
+            </Grid>
         </Box>
     );
 };
---
+++ server/controllers/userController.js
@@ -0,0 +1,84 @@
+const User = require('../models/user_schema');
+const jwt = require('jsonwebtoken');
+const Education = require('../models/acadamic_details_schema');
+const ProfessionalDetails = require('../models/professional_details_schema');
+
+//Get user profile
+exports.getProfile = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+        const data = await User.findById(userId);
+        return res.status(200).json(data);
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+//Update user profile
+exports.updateProfile = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+        const { personalData, educationData, professionalData } = req.body;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+          }
+      
+          user.firstName = personalData.firstName;
+          user.lastName = personalData.lastName;
+          user.phone = personalData.phone;
+          user.address = personalData.address;
+          user.dateOfBirth = personalData.dateOfBirth;
+      
+          if (user.acadamicDetailsKey) {
+            await Education.findByIdAndUpdate(user.acadamicDetailsKey, educationData);
+          } else {
+            const newEducation = new Education({ userId, ...educationData });
+            const savedEducation = await newEducation.save();
+            user.acadamicDetailsKey = savedEducation._id;
+          }
+      
+          if (user.profactionalDetailsKey) {
+            await ProfessionalDetails.findByIdAndUpdate(user.profactionalDetailsKey, professionalData);
+          } else {
+            const newProfessionalDetails = new ProfessionalDetails({ userId, ...professionalData });
+            const savedProfessionalDetails = await newProfessionalDetails.save();
+            user.profactionalDetailsKey = savedProfessionalDetails._id;
+          }
+      
+          await user.save();
+      
+        return res.status(200).json({personalData, educationData, professionalData});
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Get all Jobs
+exports.getallJobs = async (req, res) => {
+    try {
+        const allJobs = await Job.find();
+        res.status(200).json(allJobs);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Get Job by ID
+exports.getJobById = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId);
+        if (!job) {
+            return res.status(404).json({ error: 'Job not found' });
+        }
+        res.status(200).json(job);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
