6a58097fbb6ab6dcda16d63d5f6a27e83ac8c2cb
Srinath Bondala <srinath.b2004@gmail.com>
2024-11-08 09:45:21 +0530

    changes

--- client/src/components/AllCompanies.jsx
+++ client/src/components/AllCompanies.jsx
@@ -7,11 +7,12 @@ import axios from "axios";
 const AllCompanies = ({islogged}) => {
     const [data,setData]=useState([]);
     useEffect(() => {
-        // axios.get('https://interview-application-backend.onrender.com/user/get-all-jobs',{
+        // axios.get('http://localhost:8080/user/get-all-jobs',{
         //     headers: {
         //         Authorization: `Bearer ${localStorage.getItem('jwtToken')}`
         //     }
-        axios.get('https://interview-application-backend.onrender.com/auth/top-company').then(
+        //http://localhost:8080
+        axios.get('http://localhost:8080/auth/top-company').then(
             response => {
                 setData(response.data);
             }
